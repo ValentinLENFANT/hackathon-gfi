59eadf485ddcb3e23888444ecde17a0288e1cde4
ValentinLenfant <valentin.lenfant@hotmail.com>
2017-04-26 14:46:38 +0200

    Add the riddles and change file name for outsourcing

---
+++ helper/riddle.php
@@ -0,0 +1,19 @@
+<?php
+
+    function thirdNumber()
+    {
+        //echo !true;
+        echo ((true * 3) - (!false + (!false + true * !!!false)))*;
+    }
+    //Que retourne la fonction suivante sachant qu'elle détermine le 4ème chiffre du numéro de téléphone ?
+    function fourthNumber()
+    {
+        $i = 100;
+        while($i != chr(52))
+        {
+            $i--;
+        }
+        echo $i;
+    }
+
+    thirdNumber();
