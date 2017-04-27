7a5c34ab76c979b81afdcd0cbcf17f27e7d9e8ba
sanae <sanai-son112@hotmail.fr>
2017-04-27 16:42:15 +0200

    ghg

--- view/template/template.php
+++ view/template/template.php
@@ -12,18 +12,18 @@
     <title>Page d'accueil GFI</title>
 
     <!-- Bootstrap Core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../media/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Lato" />
     <!-- Custom Fonts -->
-    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="../media/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
 
     <!-- Plugin CSS -->
-    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+    <link href="./media/vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
 
     <!-- Theme CSS -->
-    <link href="css/creative.min.css" rel="stylesheet">
+    <link href="media/css/creative.min.css" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -35,7 +35,6 @@
 </head>
 
 <body id="page-top">
-
     <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
         <div class="container-fluid">
             <!-- Brand and toggle get grouped for better mobile display -->
@@ -43,7 +42,7 @@
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                     <span class="sr-only">Navigation</span> Menu <i class="fa fa-bars"></i>
                 </button>
-                <a class="navbar-brand page-scroll" href="#page-top"><img src="img/logo-enigma.png" style="position: relative;bottom:11px;width:45px;"></a>
+                <a class="navbar-brand page-scroll" href="#page-top"><img src="media/img/logo-enigma.png" style="position: relative;bottom:11px;width:45px;"></a>
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
@@ -56,18 +55,17 @@
                         <a class="page-scroll" href="#inscription" style="border: 1px white solid;padding-top: 2px;padding-bottom: 2px;margin-top: 12px;margin-right: 20px;">S'inscrire</a>
                     </li>
                     <li>
-                        <a class="navbar-brand page-scroll" href="#page-top"><img src="img/gfi.png" width="58" style="position: relative;bottom: 9px;"></a>
+                        <a class="navbar-brand page-scroll" href="#page-top"><img src="media/img/gfi.png" width="58" style="position: relative;bottom: 9px;"></a>
                     </li>
                 </ul>
             </div>
             <!-- /.navbar-collapse -->
         </div>
         <!-- /.container-fluid -->
-    </nav>
-
+    </nav>   
     <header>
         <div class="header-content">
-            <img src="img/logo.png" class="img-responsive centree" alt="logo enigma">
+            <img src="media/img/logo.png" class="img-responsive centree" alt="logo enigma">
             <a href="#" class="btn btn-lg btn-default video" style="border-radius:300px;border-radius: 300px;background: transparent;border: 1px solid white;color: white;"><span class="glyphicon glyphicon-play"></span>  Lire la vidéo</a>
         </div>
     </header>
@@ -87,7 +85,7 @@
   <div class="container">
     <div class="row">
       <div class="col-md-4 col-sm-6 footerleft ">
-        <div class="logofooter"><img src="img/logo-enigma.png" style="width:70px;"></div>
+        <div class="logofooter"><img src="media/img/logo-enigma.png" style="width:70px;"></div>
         <p>Enigma</p>
         <p><i class="fa fa-map-pin"></i> 7 Place Felix Eboué</p>
         <p><i class="fa fa-phone"></i> 75012, PARIS</p>
@@ -145,15 +143,15 @@
     <script src="vendor/jquery/jquery.min.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="media/vendor/bootstrap/js/bootstrap.min.js"></script>
 
     <!-- Plugin JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
-    <script src="vendor/scrollreveal/scrollreveal.min.js"></script>
-    <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+    <script src="media/vendor/scrollreveal/scrollreveal.min.js"></script>
+    <script src="media/vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
 
     <!-- Theme JavaScript -->
-    <script src="js/creative.min.js"></script>
+    <script src="media/js/creative.min.js"></script>
 
 </body>
 
